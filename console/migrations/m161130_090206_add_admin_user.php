fac235131683ab78dd7e10da27b899f849e63fed
apekin-pc\apekin <apekin@uamaster.com>
2016-12-02 14:26:49 +0200

    AdminLTE + rbac FINAL v2.0

--- console/migrations/m161130_090206_add_admin_user.php
+++ console/migrations/m161130_090206_add_admin_user.php
@@ -9,7 +9,7 @@ class m161130_090206_add_admin_user extends Migration
         $this->insert('user',[
             'id' => 1,
             'username' => 'admin',
-            'username' => Yii::$app->security->generatePasswordHash('admin'),
+            'password_hash' => Yii::$app->security->generatePasswordHash('admin'),
             'email' => 'no_email@no_email.com'
         ]);
     }
